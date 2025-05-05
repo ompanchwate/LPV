ee6f36caf69aa118b0998888d2231ea4ddaf1f7d
ompanchwate <ompanchwate2003@gmail.com>
2025-05-06 00:23:55 +0530

    updated assign1

--- HPC/sort.cpp
+++ HPC/sort.cpp
@@ -34,15 +34,13 @@ void bubbleSortParallel(vector<int> &arr)
 }
 
 // Sequential Merge Sort
-void merge(vector<int> &arr, int l, int m, int r)
+void merge(vector<int> &arr, int low, int mid, int high)
 {
-    vector<int> temp;    // temporary array
-    int left = l;      // starting index of left half of arr
-    int right = m + 1; // starting index of right half of arr
+    vector<int> temp;   
+    int left = low;      
+    int right = mid + 1;
 
-    // storing elements in the temporary array in a sorted manner//
-
-    while (left <= m && right <= r)
+    while (left <= mid && right <= high)
     {
         if (arr[left] <= arr[right])
         {
@@ -56,25 +54,22 @@ void merge(vector<int> &arr, int l, int m, int r)
         }
     }
 
-    // if elements on the left half are still left //
-
-    while (left <= m)
+    while (left <= mid)
     {
         temp.push_back(arr[left]);
         left++;
     }
 
-    //  if elements on the right half are still left //
-    while (right <= r)
+    while (right <= high)
     {
         temp.push_back(arr[right]);
         right++;
     }
 
     // transfering all elements from temporary to arr //
-    for (int i = l; i <= r; i++)
+    for (int i = low; i <= high; i++)
     {
-        arr[i] = temp[i - l];
+        arr[i] = temp[i - low];
     }
 }
 
